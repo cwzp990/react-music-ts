787275369a6677bfd3802b3aef1383c4a662356e
cwzp990 <cwzp990@163.com>
2019-03-26 17:22:21 +0800

    antd icon全局引入性能优化

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,13 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addWebpackAlias } = require('customize-cra')
+const path = require('path')
+
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: 'css',
+    style: 'css'
+  }),
+  addWebpackAlias({
+    '@ant-design/icons/lib/dist$': path.resolve(__dirname, './src/utils/icon.js')
   })
-);
+)
